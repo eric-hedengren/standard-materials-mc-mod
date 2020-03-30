fe9913c5894fa44a635c4db6be986871b2f051b0
Eric Hedengren <46898294+EricHedengren@users.noreply.github.com>
2020-03-29 20:26:39 -0600

    Add files via upload

--- java/com/baconbombingdeveloper/baconbombingmod/material/NewItemTier.java
+++ java/com/baconbombingdeveloper/baconbombingmod/material/NewItemTier.java
@@ -3,14 +3,18 @@ package com.baconbombingdeveloper.baconbombingmod.material;
 import java.util.function.Supplier;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.IItemTier;
+import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.LazyValue;
 
 public enum NewItemTier implements IItemTier {
-   OBSIDIAN(3, 3000, 9.0F, 10.0F, 25, () -> {
+   OBSIDIAN(3, 3000, 6.0F, 2.0F, 15, () -> {
       return Ingredient.fromItems(Blocks.OBSIDIAN);
+   }),
+   EMERALD(2, 500, 10.0F, 4.0F, 25, () -> {
+	  return Ingredient.fromItems(Items.EMERALD);
    });
-	
+
    private final int harvestLevel;
    private final int maxUses;
    private final float efficiency;
